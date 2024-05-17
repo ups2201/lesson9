e1694c3269db0caca8a5832e0b36354f01c692b4
ups2201 <ups2201@gmail.com>
2024-05-17 17:23:43 +0300

    refactor and add README.md

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/weather.js",
+  entry: "./src/main.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),

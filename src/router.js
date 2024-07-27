8d42c01d827f12babd8e9833969e31d2c06dc9f7
ups2201 <ups2201@gmail.com>
2024-07-27 16:18:35 +0300

    fix html and router

--- src/router.js
+++ src/router.js
@@ -3,35 +3,63 @@ import {
   RouterMode,
 } from "@amishurinskiy/router/dist/RouterFactory";
 
-const render = (content) =>
-  (document.getElementById("root").innerHTML = `<h2>${content}</h2>`);
+export let state = {
+  currentHeaderPage: "Main Page",
+  cityCurrent: "",
+  historyCity: [],
+  isAboutShow: false,
+  isMainFormShow: true,
+  isHistoryShow: false,
+};
 
-const createLogger =
-  (content, shouldRender = true) =>
-  (...args) => {
-    console.log(`LOGGER: ${content} args=${JSON.stringify(args)}`);
-    if (shouldRender) {
-      render(content);
-    }
-  };
+const render = () => {
+  document.querySelector("#main").hidden = !state.isMainFormShow;
+  document.querySelector("#about").hidden = !state.isAboutShow;
+  document.querySelector("#historyBlock").hidden = !state.isHistoryShow;
+  document.querySelector("#message").innerHTML =
+    `<h2>${state.currentHeaderPage}</h2>`;
+};
+render(state);
 
 const router = new RouterFactory().create(RouterMode.HISTORY_API);
 
 const route0 = {
   match: "/",
-  onEnter: createLogger("/"),
+  onEnter: () => {
+    state.isMainFormShow = true;
+    state.isAboutShow = false;
+    state.currentHeaderPage = "Main Page";
+    state.isHistoryShow = false;
+    render();
+  },
 };
 router.addRoute(route0);
 
 const route1 = {
   match: "/about",
-  onEnter: createLogger("/about"),
+  onEnter: () => {
+    state.isMainFormShow = false;
+    state.isAboutShow = true;
+    state.isHistoryShow = false;
+    state.currentHeaderPage = "About Page";
+    render();
+  },
 };
 router.addRoute(route1);
 
 const route2 = {
-  match: "/weather",
-  onEnter: createLogger("/weather"),
+  match: (path) => {
+    return JSON.parse(localStorage.getItem("cities"))
+      .map((city) => JSON.parse(city).name)
+      .includes(path.replace("/", ""));
+  },
+  onEnter: () => {
+    state.isMainFormShow = true;
+    state.isAboutShow = false;
+    state.isHistoryShow = true;
+    state.currentHeaderPage = "Weather Page";
+    render();
+  },
 };
 router.addRoute(route2);
 
@@ -40,9 +68,15 @@ document.body.addEventListener("click", (event) => {
     return;
   }
   event.preventDefault();
+  state.cityCurrent = event.target.innerHTML;
   const url = event.target.innerHTML;
   router.go(url, { url });
-  // unsubscribe();
+});
+
+document.querySelector("button").addEventListener("click", (event) => {
+  state.cityCurrent = document.querySelector("input").value;
+  const url = state.cityCurrent;
+  router.go(url, { url });
 });
 
 document.body.addEventListener("click", (event) => {
@@ -52,5 +86,4 @@ document.body.addEventListener("click", (event) => {
   event.preventDefault();
   const url = event.target.href;
   router.go(url, { url });
-  // unsubscribe();
 });

157e01c97959fd2cef5a1faa50e223ec48fe3ad0
ups2201 <ups2201@gmail.com>
2024-07-28 19:48:17 +0300

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { DefinePlugin } = require("webpack");
 const path = require("path");
-const PREFIX = process.env.NODE_ENV == "production" ? "/lesson9" : "/";
+const PREFIX = process.env.NODE_ENV === "production" ? "/lesson9" : "";
 
 module.exports = {
   entry: "./src/main.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: PREFIX,
+    publicPath: PREFIX + "/",
   },
   module: {
     rules: [
@@ -44,7 +44,7 @@ module.exports = {
     }),
     new DefinePlugin({
       PREFIX: JSON.stringify(PREFIX),
-      IS_PRODUCTION: process.env.NODE_ENV == "production",
+      IS_PRODUCTION: process.env.NODE_ENV === "production",
     }),
   ],
   mode: "development",
